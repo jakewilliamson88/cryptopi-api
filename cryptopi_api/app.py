35d1649ea48c66124ab5d7d6db8d48b3bcc61591
Jake Williamson <brianjw88@gmail.com>
2024-01-14 00:03:13 -0500

    Removed unused imports

--- cryptopi_api/app.py
+++ cryptopi_api/app.py
@@ -1,7 +1,6 @@
 """
 app.py Is the main file of the application.=
 """
-from http import HTTPStatus
 
 from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
@@ -24,14 +23,14 @@ app.add_middleware(
 )
 
 
-# @app.exception_handler(Exception)
-# async def validation_exception_handler(request: Request, exc: Exception):
-#     return JSONResponse(
-#         status_code=status.HTTP_400_BAD_REQUEST,
-#         content=jsonable_encoder({
-#             "detail": "Bad Request"
-#         }),
-#     )
+@app.exception_handler(Exception)
+async def validation_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({
+            "detail": "Bad Request"
+        }),
+    )
 
 
 # Register the routes.
